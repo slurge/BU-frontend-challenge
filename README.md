5d35af845cfc6e46e28f6aca24c224aea87903b2
slurge <25538133+slurge@users.noreply.github.com>
2022-04-07 22:51:14 -0500

    Agregado: layout basico

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# BU-frontend-challenge
+# Frontend-challenge
+built using Vite
+
+## Instructions
+in terminal navigate to project folder and then execute the following commands:
+- npm install
+- npm run dev
--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,54 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+function intToChar(i) {
+  return String.fromCharCode('A'.charCodeAt(0) + i);
+}
+
+const Department = ({index, data}) => {
+  return (
+    <div className="container p-3 my-4 p-md-4 department">
+      <h3>Departamento {intToChar(index)}</h3>
+      <p>{data[index]}</p>
+      <hr />
+      <div className="text-end">total</div>
+    
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  //const [departments, setDepartments] = useState({});
+  const [departments, dataHandler] = useState([
+      'a', 'c'
+  ]);
+  const [newName, setNewname] = useState('');
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+      <header >
+        <div className="container d-flex align-items-center justify-content-start h-100">
+          <button className='btn btn-primary py-2 px-4'><i class="bi bi-plus-lg"></i> Agregar departamento</button>
+        </div>
       </header>
+
+      <div className="container-fluid maincont py-3">
+
+        {
+          departments.map(
+            (_, index) => <Department index={index} data={departments} handler={dataHandler} /> 
+          )
+        }
+
+        {
+          Object.keys(departments).length === 0 && 
+          <div className="container p-3 p-md-4 department text-center">
+            <h4>Start by adding a department</h4>
+          </div>
+        }
+
+      </div>
+
     </div>
   )
 }
